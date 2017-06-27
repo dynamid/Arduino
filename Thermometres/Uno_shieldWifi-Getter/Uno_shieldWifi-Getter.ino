287b15b10680344fd95083828c736aee8a736c0d
nstouls <nicolas.stouls@insa-lyon.fr>
2017-06-27 20:21:10 +0200

    Trying some things

---
+++ Thermometres/Uno_shieldWifi-Getter/Uno_shieldWifi-Getter.ino
@@ -0,0 +1,172 @@
+
+/*
+  Web client
+
+  This sketch connects to a website (http://www.google.com)
+  using a WiFi shield.
+
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the Wifi.begin() call accordingly.
+
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the Wifi.begin() call accordingly.
+
+  Circuit:
+   WiFi shield attached
+
+  created 13 July 2010
+  by dlf (Metodo2 srl)
+  modified 31 May 2012
+  by Tom Igoe
+*/
+
+
+#include <SPI.h>
+#include <WiFi.h>
+
+char ssid[] = "P2I6"; //  your network SSID (name)
+
+int status = WL_IDLE_STATUS;
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+//char server[] = "www.google.com";    // name address for Google (using DNS)
+
+//char server[] = "y003.insa-lyon.fr";
+
+
+char devices[][18] = {"y003.insa-lyon.fr\0", // D'Alu
+                      "y005.insa-lyon.fr\0", // Nicolas
+                      "y016.insa-lyon.fr\0" // Florin
+                      //"y022.insa-lyon.fr\0", // D'Alu
+                      //"y001.insa-lyon.fr\0", // Non déployé
+                      //"y009.insa-lyon.fr\0", // Non déployé
+                      //"y017.insa-lyon.fr\0"  // Non déployé
+                     };
+
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid);
+
+    // wait 1 seconds for connection:
+    delay(1000);
+  }
+  Serial.println("Connected to wifi");
+  printWifiStatus();
+
+}
+
+void loop() {
+  String j = getJson(devices[1]);
+  Serial.print("JSON : ");
+  Serial.println(j);
+
+
+  // do nothing forevermore:
+  while (true);
+}
+
+
+String getJson(char server[]) {
+  WiFiClient client;
+
+  Serial.println("\nStarting connection to server " + String(server) + "...");
+  // if you get a connection, report back via serial:
+  //  if (client.connect(devices[0], 80)) {
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("GET /data/get HTTP/1.1");
+    client.println("Host: " + String(server));
+    client.println("Connection: close");
+    client.println();
+  }
+
+  delay(100);
+  // Waiting for a response
+  while (!client.available()) {
+    delay(10);
+  }
+
+
+  String res = client.readString();
+  // if there are incoming bytes available
+  // from the server, read them
+  while (client.available()) {
+    //char c = client.read();
+    //Serial.write(c);
+    String s = client.readString();
+    res.concat(s);
+    delay(10);
+  }
+  Serial.println(res);
+
+//  Serial.println("Raw res : " + res);
+  int start = res.indexOf("{\"value\":{");
+  if(start>=0) res = res.substring(start+9);
+
+  int last = res.indexOf(",\"response\":\"get\"}");
+  if(last >=0) res = res.substring(0,last);
+  Serial.println("From : "+String(start)+" to "+last);
+  Serial.println(res);
+
+  
+  
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting from server.");
+    client.stop();
+  }
+
+  return res;
+}
+
+
+
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+
+
+
+
